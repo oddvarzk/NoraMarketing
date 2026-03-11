4ed3676acbe272d3650ea5f115154916f7d923b8
Oddvar Zakarias Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2026-03-11 18:21:48 +0100

    bug fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,29 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Navbar from './Navbar'
 import Footer from './Footer'
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Layout() {
   const { pathname } = useLocation()
 
   useEffect(() => {
+    // Kill any pinned ScrollTrigger instances before the new page mounts
+    ScrollTrigger.getAll().forEach(st => st.kill())
     window.scrollTo({ top: 0 })
+    // Re-measure after new page components have set up their own triggers
+    const raf = requestAnimationFrame(() => ScrollTrigger.refresh())
+    return () => cancelAnimationFrame(raf)
   }, [pathname])
 
   return (
     <div className="grain flex flex-col min-h-dvh">
       <Navbar />
-      <main className="flex-1">
+      {/* key forces full unmount of old page DOM (removes GSAP pin elements) before new page mounts */}
+      <main key={pathname} className="flex-1">
         <Outlet />
       </main>
       <Footer />
